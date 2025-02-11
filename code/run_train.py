f65d1f8c7dc0f2bc0dc970b0b671cd8f2d6e3215
Tcooler <98941634+Tcooler@users.noreply.github.com>
2025-02-11 15:14:53 +0800

    Update run_train.py

--- code/run_train.py
+++ code/run_train.py
@@ -65,8 +65,8 @@ train_data,test_data = read_data(positive_file,negative_file,train_num,test_num,
 model_file = model_dir + f'{dataset}_{seed}_{k}_{lr}_{min_layernumber}_{alpha}.pt'
 result = Train(train_data,test_data,model_save_dir=model_file,device=device,pre_trained_model_dir=pre_trained_model_dir,lr=lr,mask_flag=True,alpha=alpha,min_layernumber=min_layernumber,loss_k=k,batch_size=batch_size,weight_decay=weight_decay)
 data_result = [result['accuracy'],result['auc'],result['f1'],result['tn'],result['fp'],result['fn'],result['tp']]
-data_result = [dataset,seed,k,lr,min_layernumber,alpha] + data_result
-data_column = ['dataset','seed','k','lr','min_layernumber','alpha','accuracy','auc','f1','tn','fp','fn','tp']
+data_result = [seed,k,lr,min_layernumber,alpha] + data_result
+data_column = ['seed','k','lr','min_layernumber','alpha','accuracy','auc','f1','tn','fp','fn','tp']
 data_record = pd.DataFrame([data_result],columns=data_column)
 if os.path.exists(result_file):
     data_record_ori = pd.read_csv(result_file)
